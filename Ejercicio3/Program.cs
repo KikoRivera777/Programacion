8225239f90921deed2345aa44bcba0ebff6ab9fa
KikoRiveraOficial <114604297+KikoRivera777@users.noreply.github.com>
2022-12-09 11:02:53 +0100

    Update Program.cs

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -4,7 +4,18 @@ namespace Ejercicio3
     {
         static void Main()
         {
-            
+            Console.WriteLine("Cuantas contraseñas deseas");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            Password[] password = new Password[cantidad];
+
+            Console.WriteLine("Indica la longitud deseada");
+            int longitud = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine(password);
+            }
         }
     }
 }
