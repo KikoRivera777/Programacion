6aea8c629f3561b68ea642343472273870b8b14c
KikoRiveraOficial <114604297+KikoRivera777@users.noreply.github.com>
2023-01-08 13:15:05 +0100

    Create Cancion.cs

---
+++ Ejercicio6/Cancion.cs
@@ -0,0 +1,14 @@
+namespace Ejercicio6
+{
+    class Cancion
+    {
+        private string titulo;
+        private string autor;
+
+        public Cancion (string titulo, string autor)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+        }
+    }
+}
